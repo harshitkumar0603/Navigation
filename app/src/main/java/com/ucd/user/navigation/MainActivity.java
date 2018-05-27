9974adf7725b58562b7a6a832c33a29e8cba465b
mouyangfan <15200812672@163.com>
2018-05-27 21:31:48 +0800

    test1m

--- app/src/main/java/com/ucd/user/navigation/MainActivity.java
+++ app/src/main/java/com/ucd/user/navigation/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-// commment 1 33 4
+// commment by mouyangfan
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
